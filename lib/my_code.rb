f8814b97070efe201214d1a46f378552742f7ad1
Aniece Thompson <aniecet@yahoo.com>
2019-09-29 22:04:37 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -19,11 +19,23 @@ def map_to_no_change(source_array)
 end
 
 def map_to_double(source_array)
- source_array.map { |n| n * 2}
+  final_array = []
+  i = 0
+  while i < source_array.size do
+    final_array.push(source_array[i] * 2)
+    i += 1
+  end
+  final_array
 end
 
 def map_to_square(source_array)
-  source_array.map { |n| n ** 2}
+  final_array = []
+  i = 0
+  while i < source_array.size do
+    final_array.push(source_array[i] ** 2)
+    i += 1
+  end
+  final_array
 end
 
 def reduce_to_total(source_array, starting_point)
