0532424006d16b20927f76a437648815993144b7
Aniece Thompson <aniecet@yahoo.com>
2019-09-29 21:34:12 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,21 @@
-# My Code here....
+def map_to_negativize(source_array)
+  source_array.map { |n| n * -1 }
+end
+
+def map_to_no_change(source_array)
+   source_array.map { |n| n}
+end
+
+def map_to_double(source_array)
+ source_array.map { |n| n * 2}
+end
+
+def map_to_square(source_array)
+  source_array.map { |n| n ** 2}
+end
+
+def reduce_to_total(source_array, starting_point)
+  
+  soure_array.sum(starting_point)
+end
+
